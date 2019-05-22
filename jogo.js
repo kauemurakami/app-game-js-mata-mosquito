d6816d871156cf727c3e2189193ba9d2db86c915
kauê murakami <kauetmurakami@gmail.com>
2019-05-22 13:46:30 -0300

    lado esquerdo e direito, direção da frente do mosquito

--- jogo.js
+++ jogo.js
@@ -24,13 +24,12 @@ function posicaoRandomica(){
 //criar mosquito elemento html utilizando o dom para criar o elemento img html com createElement
 var mosquito = document.createElement('img')
 mosquito.src = 'imagens/mosca.png' //acessa o elemento img e seu atributo src para definir o caminho da imagem
-mosquito.className = tamanhoRandomico() // acessando classe
+mosquito.className = tamanhoRandomico() +' '+ ladoAleatorio()// acessando classe
 mosquito.style.left = posicaoX + 'px' //acessa o style da tag e o elemento left dele atribui a pos e concatena px para que seja interpretado como pixel
 mosquito.style.top = posicaoY + 'px' // para o top
 mosquito.style.position = 'absolute'
 document.body.appendChild(mosquito)//adicionando um filho para o body com o dom	
 
-console.log(mosquito.className)
 
 
 }
@@ -44,5 +43,17 @@ function tamanhoRandomico(){
 			return 'mosquito2'
 		case 2:
 			return 'mosquito3'
+	}
+}
+
+//função responsavel para qual lado o mosquito irá olhar
+function ladoAleatorio(){
+	var classe = Math.floor(Math.random()* 2)
+
+	switch (classe) {
+		case 0:
+			return 'ladoA'
+		case 1:
+			return 'ladoB'
 	}
 }
