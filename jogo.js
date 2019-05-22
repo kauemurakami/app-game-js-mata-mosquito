d0598dbf2ab55fdd54434f261ecf3c9c87c49b5c
kauê murakami <kauetmurakami@gmail.com>
2019-05-22 13:33:14 -0300

    tamanho randomico do mosquito

--- jogo.js
+++ jogo.js
@@ -24,9 +24,25 @@ function posicaoRandomica(){
 //criar mosquito elemento html utilizando o dom para criar o elemento img html com createElement
 var mosquito = document.createElement('img')
 mosquito.src = 'imagens/mosca.png' //acessa o elemento img e seu atributo src para definir o caminho da imagem
-mosquito.className = 'mosquito1' // acessando classe
+mosquito.className = tamanhoRandomico() // acessando classe
 mosquito.style.left = posicaoX + 'px' //acessa o style da tag e o elemento left dele atribui a pos e concatena px para que seja interpretado como pixel
 mosquito.style.top = posicaoY + 'px' // para o top
 mosquito.style.position = 'absolute'
 document.body.appendChild(mosquito)//adicionando um filho para o body com o dom	
+
+console.log(mosquito.className)
+
+
+}
+//função que definira uma das nossas tres classes para mosquito
+function tamanhoRandomico(){
+	var classe = Math.floor(Math.random()* 3)
+	switch (classe) {
+		case 0:
+			return 'mosquito1'
+		case 1:
+			return 'mosquito2'
+		case 2:
+			return 'mosquito3'
+	}
 }
